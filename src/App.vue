a7fc67725adefd2f958fdd8b679482fc915be2cb
caicodes <caicodes1@gmail.com>
2022-08-14 16:31:03 -0600

    vuetify with customizations ready

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      yo
     </v-main>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-}
-</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,12 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "../styles/vuetify.scss";
 
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  theme: {
+    defaultTheme: "dark",
+  },
+});
