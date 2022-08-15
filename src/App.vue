e2164da4ff5f1d945ddfb79a552b66cb9d5d5424
caicodes <caicodes1@gmail.com>
2022-08-14 18:59:32 -0600

    firebase added as a service, init working on include, settting up pinia with it next

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
+<script setup>
+// import { firebase } from './services/firebase'
+
+const projAppName = import.meta.env.VITE_PROJECT_APP_NAME
+const siteTitle = import.meta.env.VITE_SITE_TITLE
+</script>
 <template>
   <v-app>
     <v-main>
       <!-- expo site -->
+      {{ projAppName }}
+      {{ siteTitle }}
     </v-main>
   </v-app>
 </template>
---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+}
+
+export const firebase = initializeApp(firebaseConfig)
